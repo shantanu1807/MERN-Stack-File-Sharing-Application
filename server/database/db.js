eaa0e6d01b51e736551edb08f9844177fe6cfb33
Shantanu1807 <shantanupatel140@gmail.com>
2024-04-18 17:50:26 +0530

    push folder

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb+srv://shantanupatel607:shiv18072003@file-sharing.qonuujl.mongodb.net/?retryWrites=true&w=majority&appName=File-sharing`;
+
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
